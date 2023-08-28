af5342f8d9be941e516ba6b590271bfcad0e756d
dina <timcud6@gmail.com>
2023-08-28 11:34:01 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Welcome from './Welcome'; 
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Alice" />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Welcome(props) {
+  return <p>Welcome, {props.name}!</p>;
+}
+
+export default Welcome;
